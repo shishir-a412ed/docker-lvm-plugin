a51d6629ccc1553a129285f56975531172561cf6
Shishir Mahajan <shishir.mahajan@redhat.com>
2016-02-10 14:29:56 -0500

    Added CLI and flag parsing
    Signed-off-by: Shishir Mahajan <shishir.mahajan@redhat.com>

--- src/main.go
+++ src/main.go
@@ -3,17 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 const (
 	lvmPluginSocketPath = "/var/run/docker/plugins"
+	vgConfigPath        = "/etc/sysconfig/docker-lvm-driver"
 )
 
 var (
-	flDebug  = flag.Bool("debug", false, "enable debug logging")
-	flListen = flag.String("listen", lvmPluginSocketPath+"/lvm.sock", "socket to listen for incoming connections")
+	flVgPath  *string
+	flVersion *bool
+	flListen  *string
 )
 
+func init() {
+	flVgPath = flag.String("vg-config", vgConfigPath, "Location of the volume group config file")
+	flVersion = flag.Bool("version", false, "Print version information and quit")
+	flListen = flag.String("listen", lvmPluginSocketPath+"/lvm.sock", "socket to listen for incoming connections")
+}
+
 func main() {
-	fmt.Println("Hello Docker LVM Plugin")
+
+	flag.Parse()
+
+	if *flVersion {
+		fmt.Fprint(os.Stdout, "docker lvm plugin version: 1.0\n")
+		return
+	}
+
+	fmt.Println("docker lvm plugin successful")
 }
